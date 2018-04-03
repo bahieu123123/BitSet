00006266e372c0989a27287cd2c7a86a6c8f9421
bahieu123123 <ba.hieu0207@gmail.com>
2018-04-03 21:19:53 +0300

    Lesson1

--- src/BitSet.java
+++ src/BitSet.java
@@ -8,47 +8,59 @@ public class BitSet {
     private int size;
 
     public BitSet(int size) {
-        if (size <= 0) throw new NumberFormatException();
+        if (size <= 0) throw new IllegalArgumentException();
         else this.size = size;
         if (size % 8 == 0) this.bits = new byte[size / 8];
         else this.bits = new byte[size / 8 + 1];
+    }
+    public boolean check(int element) {
+        if (element < 0 && element >= size) throw new IndexOutOfBoundsException();
+
+        int indexOfBitSet = element / 8;
+        byte numb = bits[indexOfBitSet];
+        return ((numb >> (8 * (indexOfBitSet + 1) - element - 1)) & 1) == 1;
     }
 
     @Override
     public String toString() {
         StringBuilder SB = new StringBuilder();
-        SB.append("[");
+        SB.append("{");
         for (int i = 0; i < this.size; i++) {
-            int indexofBitSet = i / 8;
-            if (((bits[indexofBitSet] >> (8 * (indexofBitSet + 1) - i - 1)) & 1) == 1) {
-                SB.append(i).append("; ");
+            if (this.check(i)==true){
+               SB.append(i).append(", ");
             }
         }
         SB.delete(SB.length() - 2, SB.length());
-        SB.append("]");
+        SB.append("}");
         return SB.toString();
     }
 
-    public void addindex(int index) {
-        if (index < 0 && index >= size) throw new IndexOutOfBoundsException();
-        int indexofBitSet = index / 8;
-        byte number = bits[indexofBitSet];
-        number = (byte) (number | (1 << (8 * (indexofBitSet + 1) - index - 1)));
-        bits[indexofBitSet] = number;
+    public boolean addindex(int index) {
+        if (!check(index)) {
+            int indexofBitSet = index / 8;
+            byte number = bits[indexofBitSet];
+            number = (byte) (number | (1 << (8 * (indexofBitSet + 1) - index - 1)));
+            bits[indexofBitSet] = number;
+            return true;
+        }
+        return false;
     }
 
 
-    public void removeindex(int index) {
-        if (index < 0 && index >= size) throw new IndexOutOfBoundsException();
-        int indexofBitSet = index / 8;
-        byte number = bits[indexofBitSet];
-        number = (byte) (number ^ (1 << (8 * (indexofBitSet + 1) - index - 1)));
-        bits[indexofBitSet] = number;
+    public boolean removeindex(int index) {
+        if (check(index)) {
+            int indexofBitSet = index / 8;
+            byte number = bits[indexofBitSet];
+            number = (byte) (number ^ (1 << (8 * (indexofBitSet + 1) - index - 1)));
+            bits[indexofBitSet] = number;
+            return true;
+        }
+        return false;
     }
 
 
     public BitSet OR(BitSet other) {
-        if (this.size != other.size) throw new NullPointerException();
+        if (this.size != other.size) throw new IllegalArgumentException();
         for (int i = 0; i < this.bits.length; i++) {
             this.bits[i] = (byte) (this.bits[i] | other.bits[i]);
         }
@@ -58,6 +70,7 @@ public class BitSet {
 
     public BitSet AND(BitSet other) {
         if (this.size != other.size) throw new NullPointerException();
+
         for (int i = 0; i < this.bits.length; i++) {
             this.bits[i] = (byte) (this.bits[i] & other.bits[i]);
         }
@@ -72,21 +85,19 @@ public class BitSet {
         return this;
     }
 
-    public int addmassive(int[] indexs) {
+    public int addMassive(int[] indexs) {
         int result = 0;
         for (int index : indexs) {
-            int indexofBitSet = index / 8;
-            if (((bits[indexofBitSet] >> (8 * (indexofBitSet + 1) - index - 1)) & 1) != 1) {
+            if (addindex(index)){
                 result++;
             }
         }
         return result;
     }
-    public int removemassive(int[] indexs){
+    public int removeMassive(int[] indexs){
         int result =0;
         for (int index : indexs){
-            int indexofBitSet=index/8;
-            if (((bits[indexofBitSet] >> (8 * (indexofBitSet + 1) - index - 1)) & 1) == 1) {
+            if (removeindex(index)){
                 result++;
             }
         }
--- src/BitSetTest.java
+++ src/BitSetTest.java
@@ -12,6 +12,18 @@ class BitSetTest {
     BitSet newBitSet1 = new BitSet(newsize);
     BitSet newBitSet2 = new BitSet(newsize);
 
+    @Test
+    public void check() {
+        bitSet1.addindex(1);
+        bitSet1.addindex(4);
+        bitSet1.addindex(5);
+        bitSet1.addindex(7);
+        assertTrue(bitSet1.check(1));
+        assertTrue(bitSet1.check(4));
+        assertTrue(bitSet1.check(5));
+        assertFalse(bitSet1.check(3));
+    }
+
 
     @Test
     public void OR() {
@@ -21,14 +33,14 @@ class BitSetTest {
             if (i % 3 == 0 && i < newsize)
                 bitSet2.addindex(i);
         }
-        assertEquals(bitSet1, bitSet1.OR(bitSet1));
+        assertEquals(bitSet1, bitSet1.OR(bitSet2));
     }
 
     @Test
     public void AND() {
         for (int i = 0; i < size; i++) {
-            if (i % 2 == 0) bitSet1.addindex(i);
-            if (i % 3 == 0) bitSet2.addindex(i);
+            if (i < 6) bitSet1.addindex(i);
+            if (i < 9) bitSet2.addindex(i);
         }
 
         assertEquals(bitSet1, bitSet1.AND(bitSet2));
@@ -38,16 +50,20 @@ class BitSetTest {
 
     @Test
     public void addindex() {
-        bitSet1.addindex(1);
-        bitSet1.addindex(4);
-        bitSet1.addindex(5);
-        bitSet1.addindex(9);
+        bitSet1.addindex(2);
+        assertTrue(bitSet1.addindex(4));
+        assertTrue(bitSet1.addindex(6));
+        assertTrue(bitSet1.addindex(8));
+        assertFalse(bitSet1.addindex(2));
     }
 
     @Test
     public void removeindex() {
-        bitSet1.removeindex(1);
-        bitSet1.removeindex(5);
+        bitSet1.addindex(2);
+        bitSet1.addindex(7);
+        assertTrue(bitSet1.removeindex(2));
+        assertTrue(bitSet1.removeindex(7));
+        assertFalse(bitSet1.removeindex(10));
     }
     @Test
     public void complement() {
@@ -68,15 +84,16 @@ class BitSetTest {
     public void addMassive() {
         bitSet1.addindex(2);
         bitSet1.addindex(7);
-        assertEquals(3, bitSet1.addmassive(new int[]{1, 7, 5, 3}));
-        assertEquals(2, bitSet1.addmassive(new int[]{5, 10}));
+        assertEquals(3, bitSet1.addMassive(new int[]{2, 3, 5, 8}));
+        assertEquals(1, bitSet1.addMassive(new int[]{7, 10}));
     }
     @Test
     public void removeMassive() {
-        bitSet1.addindex(2);
+        bitSet1.addindex(3);
         bitSet1.addindex(7);
-        assertEquals(2, bitSet1.removemassive(new int[]{2,3,5,6,7}));
-        assertEquals(1, bitSet1.removemassive(new int[]{5,4,3,2,1}));
+        bitSet1.addindex(5);
+        assertEquals(1, bitSet1.removeMassive(new int[]{9, 8, 2, 3}));
+        assertEquals(2, bitSet1.removeMassive(new int[]{7, 5, 2, 3}));
 
     }
 
